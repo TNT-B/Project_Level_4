2a39e9692f81c4c727c3c4b6e28aa47c9a33cb83
TNT-B <thanhbinh8223@gmail.com>
2023-05-16 22:55:07 +0700

    first commit

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,7 +1,117 @@
+import React, { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Menu, Popover, Row, Col } from "antd";
+import USER from "../Images/profile/pic1.jpg";
 
-const Admin = () => {
-    return (
-      <h1>Admin page</h1>
-    );
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+import logo from "../Images/logo.png";
+import logoa from "../Images/logo-a.png";
+import QLViTri from "./QLViTri/QLViTri";
+
+const { Header, Sider, Content } = Layout;
+
+export default function Admin() {
+  const [collapsed, setCollapsed] = useState({ collapsed: true });
+  const [selectedMenuItem, setSelectedMenuItem] = useState("item1");
+
+  const toggle = () => {
+    setCollapsed((collapsed) => !collapsed);
   };
-  export default Admin;
+  const componentsSwitch = (key) => {
+    switch (key) {
+      case "item1":
+        return <QLViTri />;
+      case "item2":
+        // return <QLTuyenDung />;
+        return <QLViTri />;
+      default:
+        break;
+    }
+  };
+  const content = (
+    <Menu>
+      <Menu.Item>
+        <Link to="/profile">Hồ sơ</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/" onClick={() => {}}>
+          Đăng xuất
+        </Link>
+      </Menu.Item>
+    </Menu>
+  );
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">
+          <Link to="/admin">
+            <img src={collapsed ? logoa : logo} alt="" />
+          </Link>
+        </div>
+        <Menu
+          selectedKeys={selectedMenuItem}
+          theme="dark"
+          mode="inline"
+          onClick={(e) => setSelectedMenuItem(e.key)}
+        >
+          <Menu.Item key="item1" icon={<UserOutlined />}>
+            <Link to="quan-li-vi-tri">Quản lý vị trí </Link>
+          </Menu.Item>
+          <Menu.Item key="item2" icon={<VideoCameraOutlined />}>
+            <span>Quản lý tuyển dụng</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          <Row>
+            <Col flex={1}>
+              {React.createElement(
+                collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+                {
+                  className: "trigger",
+                  onClick: toggle,
+                }
+              )}
+            </Col>
+            <Col>
+              <Popover content={content}>
+                <a href="#" className="dropdown-toggle nav-link">
+                  <span className="user-img">
+                    <img
+                      className="rounded-circle"
+                      src={USER}
+                      width={31}
+                      alt={"Binh"}
+                    />{" "}
+                    {"user.hoTen"}
+                  </span>
+                </a>
+              </Popover>
+            </Col>
+          </Row>
+        </Header>
+
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: "100vh",
+          }}
+        >
+          {/* {//componentsSwitch(selectedMenuItem)} */}
+          <Routes>
+            <Route path="quan-li-vi-tri" element={<QLViTri />} />
+          </Routes>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+}

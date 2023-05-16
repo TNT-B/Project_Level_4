f33e1a5f25b0a481d3f312a6c16afcbb2e292365
TNT-B <thanhbinh8223@gmail.com>
2023-05-16 17:29:46 +0700

    api

--- Admin/src/redux/_constants/api.constants.js
+++ Admin/src/redux/_constants/api.constants.js
@@ -3,7 +3,6 @@ const BASE_URL = base_api.URL;
 export const apiConstants = {
   //quản lý vi tri
 
-  USER_SEARCH: "",
   DANH_SACH_VI_TRI: "https://quan-ly-tuyen-dung-be.onrender.com/vitri?term=fr",
   CAP_NHAT_VI_TRI: "https://quan-ly-tuyen-dung-be.onrender.com/vitri",
   TAO_VI_TRI: "https://quan-ly-tuyen-dung-be.onrender.com/vitri/",
